b512bbfd80f96947f4ea89ae9eba9d5068784ac3
Fabian Hilz <fabian.hilz@t-online.de>
2022-04-17 20:11:51 +0200

    Add capacitor and ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wapp-solutions.ref-session',
+  appName: 'ref-session',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
