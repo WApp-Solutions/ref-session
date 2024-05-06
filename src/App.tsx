d3b9b56f7e18da8479c3130d73f51911a4b0bdb4
Fabian Menzel <fabian.menzel@wapp-solutions.com>
2024-05-06 23:26:03 +0200

    (#3) Adapt different paths in different environments

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './theme/variables.scss'
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import '@ionic/react/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
 
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
@@ -36,7 +34,9 @@ const App: FC = () => {
             <IonApp>
                 <IonReactRouter>
                     <IonRouterOutlet>
-                        <Route path="/register/session/:sessionID">
+                        <Route
+                            path={`${process.env.PUBLIC_URL}/register/session/:sessionID`}
+                        >
                             <Session />
                         </Route>
                     </IonRouterOutlet>
